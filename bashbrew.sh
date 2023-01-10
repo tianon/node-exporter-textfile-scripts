c4dfefb630ab79e1a1aa83fb6370a0630c30e028
Tianon Gravi <admwiggin@gmail.com>
2023-01-09 17:21:07 -0800

    Add a script that generates some basic "bashbrew" stats

---
+++ bashbrew.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+if [ "$#" -eq 0 ]; then
+	set -- ':'
+fi
+
+for namespaceLibrary; do
+	[[ "$namespaceLibrary" == *:* ]]
+
+	export BASHBREW_NAMESPACE="${namespaceLibrary%%:*}"
+	[ "$BASHBREW_NAMESPACE" != "$namespaceLibrary" ]
+
+	export BASHBREW_LIBRARY="${namespaceLibrary#$BASHBREW_NAMESPACE:}"
+	[ "$BASHBREW_LIBRARY" != "$namespaceLibrary" ]
+
+	if [ -z "$BASHBREW_LIBRARY" ]; then
+		unset BASHBREW_LIBRARY
+	fi
+
+	labels="namespace=\"$BASHBREW_NAMESPACE\""
+
+	repos="$(bashbrew list --all --repos)"
+
+	repo_count="$(wc -l <<<"$repos")"
+	echo "bashbrew_repo_count{$labels} $repo_count"
+
+	for repo in --all $repos; do (
+		if [ "$repo" != '--all' ]; then
+			labels="$labels,repo=\"$repo\""
+			repoName="$(basename "$repo")"
+		else
+			repoName="$repo"
+		fi
+
+		arches="$(bashbrew cat --format '{{ range .Entries }}{{ .Architectures | join "\n" }}{{ "\n" }}{{ end }}' "$repoName" | sort -u)"
+
+		archCount="$(wc -l <<<"$arches")"
+		echo "bashbrew_arch_count{$labels} $archCount"
+
+		for arch in '' $arches; do (
+			args=()
+			if [ -n "$arch" ]; then
+				labels="$labels,arch=\"$arch\""
+				export BASHBREW_ARCH="$arch"
+				args+=( --arch-filter )
+			fi
+
+			uniq="$(bashbrew list "${args[@]}" --uniq "$repoName" | wc -l)"
+			echo "bashbrew_uniq_count{$labels} $uniq"
+
+			tags="$(bashbrew list "${args[@]}" "$repoName" | sort -u | wc -l)"
+			echo "bashbrew_tag_count{$labels} $tags"
+		) done
+	) done
+done
